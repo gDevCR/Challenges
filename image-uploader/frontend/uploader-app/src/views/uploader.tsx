3474623bf6fc281e5cb12d71308aecd0339075bc
GerSC <gersc10@gmail.com>
2022-02-20 04:50:32 -0600

    Validate environment
    Validate environment

--- image-uploader/frontend/uploader-app/src/views/uploader.tsx
+++ image-uploader/frontend/uploader-app/src/views/uploader.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Alert, ProgressBar } from 'react-bootstrap';
 import './uploader.scss';
 import ImageUploader from '../assets/imgs/image.svg';
-import { isProduction } from '../Utils';
+import { isProduction } from '../utilitaries';
 
 interface props {
   resultStatus: (result: boolean) => void;
